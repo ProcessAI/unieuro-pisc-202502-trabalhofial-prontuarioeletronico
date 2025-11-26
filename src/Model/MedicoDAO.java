f03ec528ac222202c394030d2c43334c871e13ac
LuizCMaia <luizmiguel.maia05@gmail.com>
2025-11-26 11:54:16 -0300

    MedicoDAO.java

--- src/Model/MedicoDAO.java
+++ src/Model/MedicoDAO.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class MedicoDAO {
 
-    // Método para SALVAR um novo médico no PostgreSQL
+    
     public void salvar(Medico medico) throws SQLException {
         String sql = "INSERT INTO medicos (crm, nome, especialidade, telefone, email) VALUES (?, ?, ?, ?, ?)";
         try (Connection conexao = Conexao.getConnection();
@@ -23,7 +23,7 @@ public class MedicoDAO {
         }
     }
 
-    // Método para BUSCAR um médico pelo CRM
+ 
     public Medico buscarPorCrm(String crm) throws SQLException {
         String sql = "SELECT nome, especialidade, telefone, email FROM medicos WHERE crm = ?";
         try (Connection conexao = Conexao.getConnection();
@@ -45,7 +45,7 @@ public class MedicoDAO {
         }
     }
 
-    // Método para ATUALIZAR os dados de um médico
+   
     public void atualizar(Medico medico) throws SQLException {
         String sql = "UPDATE medicos SET nome = ?, especialidade = ?, telefone = ?, email = ? WHERE crm = ?";
         try (Connection conexao = Conexao.getConnection();
@@ -55,13 +55,13 @@ public class MedicoDAO {
             stmt.setString(2, medico.getEspecialidade());
             stmt.setString(3, medico.getTelefone());
             stmt.setString(4, medico.getEmail());
-            stmt.setString(5, medico.getCrm()); // Onde o CRM é a chave
+            stmt.setString(5, medico.getCrm()); 
             
             stmt.executeUpdate();
         }
     }
 
-    // Método para EXCLUIR um médico pelo CRM
+    
     public void excluir(String crm) throws SQLException {
         String sql = "DELETE FROM medicos WHERE crm = ?";
         try (Connection conexao = Conexao.getConnection();
@@ -71,4 +71,5 @@ public class MedicoDAO {
             stmt.executeUpdate();
         }
     }
+
 }
